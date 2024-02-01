0d526459d716de8f9579ed568a4eeae3f77ff09b
Karoline Barreto <karolinedbarreto@gmail.com>
2024-02-01 00:11:53 -0300

    feat(theme): add colors

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,17 @@
 const theme = {
-    typography: {
-        fontFamily: "'Rubik', sans-serif"
-    }
+  typography: {
+    fontFamily: "'Rubik', sans-serif",
+  },
+  colors: {
+    neutral: {
+      300: '#A6A6A6',
+      500: '#737373',
+    },
+    black: {
+      500: '#000000',
+    },
+    white: '#ffffff',
+  },
 };
 
 export default theme;
